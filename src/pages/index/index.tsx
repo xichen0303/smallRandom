e369205b089e0b87eaeea83b84e0024a73e64a1b
xichen0303 <1622348789@qq.com>
2024-01-15 01:55:02 +0800

    feat: 页面布局

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -23,29 +23,34 @@ export default function Index() {
 
   return (
     <View className='index'>
-      <View className="topContent">
-        预留位置
+      <View className='content'>
+        <View className="topContent">
+          预留位置
+        </View>
+        <View className='middleContent'>
+          <AtTabs current={state.atTabBarCurrent} tabList={[]} onClick={() => {}}>
+            <AtTabsPane current={state.atTabBarCurrent} index={0} >
+              <View style='height: 100%;' >标签页一的内容</View>
+            </AtTabsPane>
+            <AtTabsPane current={state.atTabBarCurrent} index={1}>
+              <View style='height: 100%;'>标签页二的内容</View>
+            </AtTabsPane>
+            {/* <AtTabsPane current={state.atTabBarCurrent} index={2}>
+              <Mine></Mine>
+            </AtTabsPane> */}
+          </AtTabs>
+        </View>
+        <View className='bottomContent'>
+          预留位置
+        </View>
       </View>
-      <AtTabs current={state.atTabBarCurrent} tabList={[]} onClick={() => {}}>
-        <AtTabsPane current={state.atTabBarCurrent} index={0} >
-          <View style='padding: 100px 50px;background-color: #FAFBFC;text-align: center;' >标签页一的内容</View>
-        </AtTabsPane>
-        <AtTabsPane current={state.atTabBarCurrent} index={1}>
-          <View style='padding: 100px 50px;background-color: #FAFBFC;text-align: center;'>标签页二的内容</View>
-        </AtTabsPane>
-        <AtTabsPane current={state.atTabBarCurrent} index={2}>
-          <Mine></Mine>
-        </AtTabsPane>
-      </AtTabs>
-      <View>
-        预留位置
-      </View>
+      
       <AtTabBar
         fixed
         tabList={[
-          { title: '工具', iconType: 'list'},
-          { title: '游戏', iconPrefixClass: 'fa', iconType: 'game' },
-          { title: '我的', iconType: 'user'}
+          { title: '小工具', iconType: 'list'},
+          { title: '小游戏', iconPrefixClass: 'fa', iconType: 'game' },
+          // { title: '我的', iconType: 'user'}
         ]}
         onClick={handleAtTabBarClick}
         current={state.atTabBarCurrent}
